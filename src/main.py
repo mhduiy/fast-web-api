d13cc492d298ea7419cbd851e041fcb341cbc578
zhangkun <mhduiy@qq.com>
2024-05-26 17:01:43 +0800

    feat: 增加常用刷机工具网址

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 import configparser
 from pathlib import Path
+import json
 
 app = FastAPI()
 
@@ -13,6 +14,15 @@ def bingWallpaper():
     config.read(cache_name)
     url = config.get("Wallpaper", "url")
     return {"url": url}
+
+@app.get("/flashurls")
+def bingWallpaper():
+    home_dir = str(Path.home())
+    cache_name = home_dir + "/flashUrl.json"
+    with open(cache_name, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
+    
  
 if __name__ == '__main__':
     uvicorn.run(app='main:app', host="127.0.0.1", port=1234, reload=True)
